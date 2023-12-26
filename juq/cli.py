87baa42931e12374bdd984460dfe08466dc560c5
Ryan Williams <ryan@runsascoded.com>
2023-12-25 22:06:06 -0500

    juq merge-outputs

--- juq/cli.py
+++ juq/cli.py
@@ -1,7 +1,9 @@
 import json
 from contextlib import contextmanager
 from fileinput import FileInput
-from sys import stdin
+from functools import wraps
+from inspect import getfullargspec
+from sys import stdin, stdout, stderr
 
 import click
 import nbformat
@@ -17,20 +19,37 @@ def identity(obj):
     yield obj
 
 
+def with_nb(func):
+    spec = getfullargspec(func)
+
+    @wraps(func)
+    @click.argument('nb_path', required=False)
+    def wrapper(nb_path, *args, **kwargs):
+        ctx = identity(stdin) if nb_path == '-' or nb_path is None else open(nb_path, 'r')
+        with ctx as f:
+            # nb = nbformat.read(f, as_version=4)
+            nb = json.load(f)
+        # ctx = identity(stdin) if nb_path == '-' else open(nb_path, 'r')
+        # with ctx as f:
+        #     nb = nbformat.read(f, as_version=4)
+        if 'nb_path' in spec.args:
+            kwargs['nb_path'] = nb_path
+        func(*args, nb=nb, **kwargs)
+        # with ctx as f:
+        #     nbformat.write(nb, f)
+    return wrapper
+
+
+# @cli.command()
 @cli.command()
 @click.option('-m/-M', '--metadata/--no-metadata', is_flag=True)
 @click.option('-o/-O', '--outputs/--no-outputs', default=None)
 @click.option('-s/-S', '--source/--no-source', default=None)
 @click.option('-t', '--cell-type')
 @click.argument('cells_slice')
-@click.argument('nb_path', required=False)
-def cells(cell_type, cells_slice, nb_path, **flags):
-    ctx = identity(stdin) if nb_path == '-' or nb_path is None else open(nb_path, 'r')
-    with ctx as f:
-        # nb = nbformat.read(f, as_version=4)
-        nb = json.load(f)
-        cells = nb['cells']
-
+@with_nb
+def cells(cell_type, cells_slice, nb, **flags):
+    cells = nb['cells']
     if cell_type:
         cells = [
             cell
@@ -80,6 +99,58 @@ def cells(cell_type, cells_slice, nb_path, **flags):
         print(obj)
     else:
         print(json.dumps(obj, indent=2))
+
+
+def merge_cell_outputs(cell):
+    if 'outputs' not in cell:
+        return cell
+    outputs = cell['outputs']
+    new = []
+    prv = None
+    for cur in outputs:
+        if (
+                prv and
+                prv['output_type'] == "stream" and
+                cur['output_type'] == "stream" and
+                prv.get('name') and
+                prv.get('name') == cur.get('name')
+        ):
+            prv_keys = set(prv.keys())
+            cur_keys = set(cur.keys())
+            keys = { 'output_type', 'name', 'text' }
+            if prv_keys != keys:
+                stderr.write(f"Unrecognized keys in prv: {prv_keys - keys}\n")
+            elif cur_keys != keys:
+                stderr.write(f"Unrecognized keys in cur: {cur_keys - keys}\n")
+            else:
+                prv['text'] += cur['text']
+                prv = cur
+                continue
+        new.append(cur)
+        prv = cur
+    cell['outputs'] = new
+    return cell
+
+
+@cli.command('merge-outputs')
+@click.option('-i', '--in-place', is_flag=True)
+@click.option('-o', '--out-path')
+@with_nb
+def merge_outputs(nb, nb_path, in_place, out_path):
+    if in_place:
+        if out_path:
+            raise ValueError("Cannot use `-i` with `-o`")
+        if not nb_path or nb_path == '-':
+            raise ValueError("Cannot use `-i` without explicit `nb_path`")
+        out_path = nb_path
+    nb['cells'] = [
+        merge_cell_outputs(cell)
+        for cell in nb['cells']
+    ]
+
+    ctx = identity(stdout) if out_path == '-' or out_path is None else open(out_path, 'w')
+    with ctx as f:
+        json.dump(nb, f, indent=2)
 
 
 if __name__ == '__main__':
