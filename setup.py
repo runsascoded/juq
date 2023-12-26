cf728b80dbd58dae1ad79589cd6899ee86d02aba
Ryan Williams <ryan@runsascoded.com>
2023-12-26 12:34:50 -0500

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='juq.py',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=open('requirements.txt').read(),
     extras_require={
