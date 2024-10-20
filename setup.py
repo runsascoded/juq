e798335b3b70945dc48f88db5c00b0b500047484
Ryan Williams <ryan@runsascoded.com>
2024-10-20 12:31:22 -0400

    v0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='juq.py',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     install_requires=open('requirements.txt').read(),
     extras_require={
