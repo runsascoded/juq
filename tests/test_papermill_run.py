77b0cdc9ab4e82ef0c6768f6041d38b374a32a1d
Ryan Williams <ryan@runsascoded.com>
2025-01-24 12:35:03 -0500

    `test_papermill_run`: factor cases

--- tests/test_papermill_run.py
+++ tests/test_papermill_run.py
@@ -1,35 +1,40 @@
+from __future__ import annotations
+
 import json
-from os.path import join
+from os.path import join, basename
 from tempfile import TemporaryDirectory
 
 from juq.papermill.run import papermill_run_cmd
 from tests.test_papermill_clean import TEST_DIR
 
 
-def test_mixed_tags():
-    name = "mixed-tags.ipynb"
-    before_path = join(TEST_DIR, name)
-    with open(before_path, 'r') as f:
-        before = json.load(f)
+def check(
+    in_name: str,
+    expected_name: str | None = None,
+    **kwargs,
+):
+    in_path = join(TEST_DIR, in_name)
+    if expected_name is None:
+        expected_name = in_name
+    expected_path = join(TEST_DIR, expected_name)
+    name = basename(expected_path)
     with TemporaryDirectory() as tmpdir:
-        after_path = join(tmpdir, name)
-        papermill_run_cmd.callback((before_path,), out_path=after_path)
-        with open(after_path, 'r') as f:
-            after = json.load(f)
+        out_path = join(tmpdir, name)
+        papermill_run_cmd.callback((in_path, out_path), **kwargs)
+        with open(out_path, 'r') as f:
+            actual = json.load(f)
+    with open(expected_path, 'r') as f:
+        expected = json.load(f)
+    assert actual == expected
 
-    assert after == before
+
+def test_mixed_tags():
+    check("mixed-tags.ipynb")
 
 
 def test_mixed_tags_params():
-    before_path = join(TEST_DIR, "mixed-tags-params.ipynb")
-    name = "mixed-tags-params-222.ipynb"
-    expected_path = join(TEST_DIR, name)
-    with open(expected_path, 'r') as f:
-        expected = json.load(f)
-    with TemporaryDirectory() as tmpdir:
-        after_path = join(tmpdir, name)
-        papermill_run_cmd.callback((before_path,), parameter_strs=("num=222",), out_path=after_path)
-        with open(after_path, 'r') as f:
-            actual = json.load(f)
-
-    assert actual == expected
+    check(
+        "mixed-tags-params.ipynb",
+        "mixed-tags-params-222.ipynb",
+        parameter_strs=("num=222",),
+    )
